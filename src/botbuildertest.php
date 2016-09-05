d268fe2f184450a4bb2a1f524ea9e9ca185a0e8c
peter279k <peter279k@gmail.com>
2016-09-06 06:55:02 +0800

    add json string (testing)

--- src/botbuildertest.php
+++ src/botbuildertest.php
@@ -140,6 +140,29 @@
                     )
                 )
             );
+
+            $data = '{
+  "setting_type" : "call_to_actions",
+  "thread_state" : "existing_thread",
+  "call_to_actions":[
+    {
+      "type":"postback",
+      "title":"Help",
+      "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+    },
+    {
+      "type":"postback",
+      "title":"Start a New Order",
+      "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
+    },
+    {
+      "type":"web_url",
+      "title":"View Website",
+      "url":"http://petersapparel.parseapp.com/"
+    }
+  ]
+}';
+
             return $data;
         }
 
