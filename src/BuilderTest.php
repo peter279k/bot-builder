ed34187d91c666a8e999469893462b8074963434
peter279k <peter279k@gmail.com>
2016-09-11 22:44:56 +0800

    complete the unit testing

--- src/BuilderTest.php
+++ src/BuilderTest.php
@@ -1,5 +1,5 @@
 <?php
-    require __DIR__ . "/vendor/autoload.php";
+    require "BotBuilder.php";
 
     /*
     testing:
@@ -14,31 +14,95 @@
     class BuilderTest extends PHPUnit_Framework_TestCase{
         /** @test */
         public function processTest() {
+            //subscribeTest
+            $expect = true;
+            $response = $this -> subscribeTest();
+            $this -> assertSame($expect, $response);
 
-        }
+            //addGreetingTest
+            $expect = true;
+            $response = $this -> addGreetingTest("Hello World!");
+            $this -> assertSame($expect, $response);
 
-        public function verifyTest() {
+            //delGreetingTest
+            $expect = true;
+            $response = $this -> delGreetingTest();
+            $this -> assertSame($expect, $response);
+
+            //addMenuTest
+            $menus = array(
+                 array(
+                     "type" => "postback",
+                     "title" => "Help(幫助)",
+                     "payload" => "DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+                 ),
+                 array(
+                     "type" => "postback",
+                     "title" => "About(關於)",
+                     "payload" => "DEVELOPER_DEFINED_PAYLOAD_FOR_ABOUT"
+                 )
+             );
+            $expect = true;
+            $response = $this -> addMenuTest($menus);
+            $this -> assertSame($expect, $response);
+
+            //delMenuTest
+            $expect = true;
+            $response = $this -> delMenuTest();
+            $this -> assertSame($expect, $response);
 
         }
 
         public function subscribeTest() {
+            $tokens = $this -> getToken();
+            if($tokens === false) {
+                exit("token.json file not found");
+            }
 
+            $builder = new BotBuilder($tokens["token"], $tokens["page_token"]);
+            $response = $builder -> subscribe();
+            return $response;
         }
 
-        public function addGreetingTest() {
+        public function addGreetingTest($greetingText) {
+            $tokens = $this -> getToken();
+            if($tokens === false) {
+                exit("token.json file not found");
+            }
 
+            $builder = new BotBuilder($tokens["token"], $tokens["page_token"]);
+            $response = $builder -> addGreeting();
+            return $response;
         }
 
         public function delGreetingTest() {
-
+            $tokens = $this -> getToken();
+            $builder = new BotBuilder($tokens["token"], $tokens["page_token"]);
+            $response = $builder -> delGreeting();
+            return $response;
         }
 
-        public function addMenuTest() {
-
+        public function addMenuTest($menus) {
+            $tokens = $this -> getToken();
+            $builder = new BotBuilder($tokens["token"], $tokens["page_token"]);
+            $response = $builder -> addMenu($menus);
+            return $response;
         }
 
         public function delMenuTest() {
-            
+            $tokens = $this -> getToken();
+            $builder = new BotBuilder($tokens["token"], $tokens["page_token"]);
+            $response = $builder -> delMenu();
+            return $response;
+        }
+
+        public function getToken() {
+            if(file_exists("token.json")) {
+                return $tokens = file_get_contents("token.json");
+            }
+            else {
+                return false;
+            }
         }
     }
 
